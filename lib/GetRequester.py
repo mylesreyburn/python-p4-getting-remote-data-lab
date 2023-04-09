2001bef39a58add172f90cca7ab693fda7a87954
Myles Reyburn <mylesreyburn@gmail.com>
2023-04-09 16:59:03 -0600

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = self.url
+        print(url)
+
+        response = requests.get(url)
+        response_content = response.content
+        print(response_content)
+        return response_content
 
     def load_json(self):
-        pass
+        unrefined_json = self.get_response_body()
+
+        refined_json = json.loads(unrefined_json)
+
+        return refined_json
+
+print 
